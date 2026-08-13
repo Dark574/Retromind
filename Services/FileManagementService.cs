bd983e2f72d09edda9fcb0f3a89fdc83d9f4a04e
Dark574 <markus.herkommer@gmx.de>
2026-08-13 09:29:25 +0200

    fixed: an error when deleting files is now registered

--- Services/FileManagementService.cs
+++ Services/FileManagementService.cs
@@ -187,6 +187,7 @@ public class FileManagementService
         catch (Exception ex)
         {
             Console.WriteLine($"Error deleting asset: {ex.Message}");
+            throw;
         }
 
         RaiseLibraryChanged();
