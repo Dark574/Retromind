4de2718dfdfef7b972278fe1ab15d61918fda0da
Dark574 <21107465+Dark574@users.noreply.github.com>
2026-09-16 16:12:51 +0200

    changed: make information dialogs copyable

--- Views/InfoView.axaml.cs
+++ Views/InfoView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
@@ -14,6 +15,18 @@ public partial class InfoView : Window
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+
+    private async void OnCopyClick(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not string message)
+            return;
+
+        var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+        if (clipboard == null)
+            return;
+
+        await clipboard.SetTextAsync(message);
     }
 
     private void OnOkClick(object? sender, RoutedEventArgs e)
